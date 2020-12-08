794ec1dc773b6b5fc74d9827bb6c9c5ed2ff7a3a
Afam <afam262@gmail.com>
2020-12-07 23:44:04 -0600

    html: change write up and add projects; css: add styling and js: change the functionality

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-console.log("afam")
-
+let themeDots = $('.theme-dot')
 let theme = localStorage.getItem('theme')
+
+// theme option function
+
+function setTheme(mode){
+    if(mode == 'light'){       
+        $('#theme-style').attr('href', 'style.css') 
+    }
+
+    if(mode == 'blue'){
+        $('#theme-style').attr('href', 'blue.css') 
+        
+    }
+
+    if(mode == 'green'){
+       $('#theme-style').attr('href', 'green.css') 
+    }
+
+    if(mode == 'purple'){        
+        $('#theme-style').attr('href', 'purple.css')         
+    }
+
+    for (var index=0; themeDots.length > index; index++) {
+        themeDots[index].addEventListener('click', function(){
+            let mode = this.dataset.mode
+            console.log('option clicked:', mode)
+            setTheme(mode)
+        }) 
+    }
+
+    localStorage.setItem('theme', mode)    
+}
+
+// Local storage section
 
 if(theme == null) {
     setTheme('light')
@@ -8,34 +40,3 @@ if(theme == null) {
     setTheme(theme)
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
-
-for (var i=0; themeDots.length > i; i++) {
-    themeDots[i].addEventListener('click', function(){
-        let mode = this.dataset.mode
-        console.log('option clicked:', mode)
-        setTheme(mode)
-    }) 
-}
-
-function setTheme(mode){
-    if(mode == 'light'){
-        document.getElementById('theme-style').href = 'style.css'
-    }
-
-    if(mode == 'blue'){
-        document.getElementById('theme-style').href = 'blue.css'
-    }
-
-    if(mode == 'green'){
-        document.getElementById('theme-style').href = 'green.css'
-    }
-
-    if(mode == 'purple'){
-        document.getElementById('theme-style').href = 'purple.css'
-    }
-
-    
-
-    localStorage.setItem('theme', mode)
-}
